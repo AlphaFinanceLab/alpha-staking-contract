ca21f5008e5adc5fa26f2511897707d55a8bbb06
Griphook <griphook.eth@protonmail.com>
2021-02-16 15:03:37 +0000

    First version of AlphaStaking

--- contracts/AlphaStaking.sol
+++ contracts/AlphaStaking.sol
@@ -1,17 +1,78 @@
 pragma solidity 0.6.12;
 
-contract AlphaStaking {
-  // TODO
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC20/IERC20.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/token/ERC20/SafeERC20.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/utils/ReentrancyGuard.sol';
+import 'OpenZeppelin/openzeppelin-contracts@3.2.0/contracts/math/SafeMath.sol';
 
-  function stake() external {
-    // TODO
+contract AlphaStaking is ReentrancyGuard {
+  using SafeERC20 for IERC20;
+  using SafeMath for uint;
+
+  uint public constant STATUS_READY = 0;
+  uint public constant STATUS_UNBONDING = 1;
+  uint public constant UNBONDING_DURATION = 7 days;
+  uint public constant WITHDRAW_DURATION = 1 days;
+
+  struct Data {
+    uint status;
+    uint share;
+    uint unbondTime;
+    uint unbondShare;
   }
 
-  function unbond() external {
-    // TODO
+  IERC20 public alpha;
+  uint public totalAlpha;
+  uint public totalShare;
+  mapping(address => Data) public users;
+
+  function stake(uint amount) external nonReentrant {
+    require(amount >= 1e18, 'stake/amount-too-small');
+    Data storage data = users[msg.sender];
+    require(data.status == STATUS_READY, 'stake/not-ready');
+    alpha.safeTransferFrom(msg.sender, address(this), amount);
+    uint share = totalAlpha == 0 ? amount : amount.mul(totalShare).div(totalAlpha);
+    totalAlpha = totalAlpha.add(amount);
+    totalShare = totalShare.add(share);
+    data.share = data.share.add(share);
   }
 
-  function withdraw() external {
-    // TODO
+  function unbond(uint share) external nonReentrant {
+    Data storage data = users[msg.sender];
+    require(data.status == STATUS_READY, 'unbond/not-ready');
+    require(share <= data.share, 'unbond/insufficient-share');
+    data.status = STATUS_UNBONDING;
+    data.unbondTime = block.timestamp;
+    data.unbondShare = share;
+  }
+
+  function withdraw() external nonReentrant {
+    Data storage data = users[msg.sender];
+    require(data.status == STATUS_UNBONDING, 'withdraw/not-unbonding');
+    require(block.timestamp >= data.unbondTime + UNBONDING_DURATION, 'withdraw/not-valid');
+    require(
+      block.timestamp < data.unbondTime + UNBONDING_DURATION + WITHDRAW_DURATION,
+      'withdraw/already-expired'
+    );
+    uint share = data.unbondShare;
+    uint amount = totalAlpha.mul(share).div(totalShare);
+    totalAlpha = totalAlpha.sub(amount);
+    totalShare = totalShare.sub(share);
+    data.share = data.share.sub(share);
+    data.unbondTime = 0;
+    data.unbondShare = 0;
+    alpha.safeTransfer(msg.sender, amount);
+  }
+
+  function reset() external nonReentrant {
+    Data storage data = users[msg.sender];
+    require(data.status == STATUS_UNBONDING, 'reset/not-unbonding');
+    require(
+      block.timestamp >= data.unbondTime + UNBONDING_DURATION + WITHDRAW_DURATION,
+      'reset/not-expired'
+    );
+    data.status = STATUS_READY;
+    data.unbondTime = 0;
+    data.unbondShare = 0;
   }
 }
