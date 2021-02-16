63a730eb7b720fa611c746da52503eb6d8ab6569
Griphook <griphook.eth@protonmail.com>
2021-02-16 11:45:27 +0000

    Add staking template

---
+++ contracts/AlphaStaking.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.6.12;
+
+contract AlphaStaking {
+  // TODO
+
+  function stake() external {
+    // TODO
+  }
+
+  function unbond() external {
+    // TODO
+  }
+
+  function withdraw() external {
+    // TODO
+  }
+}
