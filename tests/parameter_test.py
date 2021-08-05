7b3d5ab94a145c86eedc6e7b48ba88653df2659c
hermioneeth <hermioneeth@gmail.com>
2021-08-05 14:54:41 +0700

    Fix test cases for update withdraw duration

--- tests/parameter_test.py
+++ tests/parameter_test.py
@@ -6,7 +6,6 @@ import brownie
 def test_unchange_parameter_after_upgrade(staking, staking_v2, proxy_admin, alice):
     prev_STATUS_READY = staking.STATUS_READY()
     prev_STATUS_UNBONDING = staking.STATUS_UNBONDING()
-    prev_WITHDRAW_DURATION = staking.WITHDRAW_DURATION()
     prev_alpha = staking.alpha()
     prev_governor = staking.governor()
     prev_pendingGovernor = staking.pendingGovernor()
@@ -18,27 +17,33 @@ def test_unchange_parameter_after_upgrade(staking, staking_v2, proxy_admin, alic
     proxy_admin.upgrade(staking, staking_v2)
 
     assert prev_STATUS_READY == staking.STATUS_READY(), "STATUS_READY must be the same"
-    assert prev_STATUS_UNBONDING == staking.STATUS_UNBONDING(
+    assert (
+        prev_STATUS_UNBONDING == staking.STATUS_UNBONDING()
     ), "STATUS_UNBONDING must be the same"
-    assert prev_WITHDRAW_DURATION == staking.WITHDRAW_DURATION(
-    ), "WITHDRAW_DURATION must be the same"
     assert prev_alpha == staking.alpha(), "alpha must be the same"
     assert prev_governor == staking.governor(), "governor must be the same"
-    assert prev_pendingGovernor == staking.pendingGovernor(
+    assert (
+        prev_pendingGovernor == staking.pendingGovernor()
     ), "pendingGovernor must be the same"
     assert prev_worker == staking.worker(), "worker must be the same"
     assert prev_totalAlpha == staking.totalAlpha(), "totalAlpha must be the same"
     assert prev_totalShare == staking.totalShare(), "totalShare must be the same"
     assert prev_admin == proxy_admin.getProxyAdmin(
-        staking), "proxy admin must be the same"
+        staking
+    ), "proxy admin must be the same"
 
 
 def test_change_parameter_after_upgrade(staking, staking_v2, proxy_admin, alice):
     prev_UNBONDING_DURATION = staking.UNBONDING_DURATION()
+    prev_WITHDRAW_DURATION = staking.WITHDRAW_DURATION()
     prev_impl = proxy_admin.getProxyImplementation(staking)
 
     proxy_admin.upgrade(staking, staking_v2)
 
-    assert prev_UNBONDING_DURATION != staking.UNBONDING_DURATION(
+    assert (
+        prev_UNBONDING_DURATION != staking.UNBONDING_DURATION()
     ), "UNBONDING_DURATION must change"
+    assert (
+        prev_WITHDRAW_DURATION != staking.WITHDRAW_DURATION()
+    ), "WITHDRAW_DURATION must change"
     assert prev_impl != proxy_admin.getProxyImplementation(staking)
